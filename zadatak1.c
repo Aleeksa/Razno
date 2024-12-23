ed4d74d6d493b5071bcd5890913d45c7f5638fae
Алекса Антић <aleksa.antic1205@gmail.com>
2024-12-23 03:03:55 +0100

    update

--- zadatak1.c
+++ zadatak1.c
@@ -8,15 +8,38 @@ int fib(int n){
         return fib(n-1)+fib(n-2);
     }
 }
-void racun(int a[][100], int n){
+void gore(int a[][100], int n){
     int br = 0;
-        for(int j = n-1 ; j >= 0 ; j--){
+    int j;
+        for(j = n-1 ; j >= 0 ; j--){
             a[0][j] = fib(n-br);
+                
             if(a[0][j] > 0){
-                a[0][j] %= 10;
+               // a[0][j] %= 10;
             }
             br++;
         }
+}
+void sporedna(int a[][100], int n){
+    int br = 0;
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < n ; j++){
+            if(i+j == n-1){
+                a[i][j]=fib(n+br);
+                if(a[i][j]>0){
+                    //a[i][j] %= 10;
+                }
+                br++;
+            }
+        }
+    }
+}
+void dole(int a[][100], int n){
+    int br = n-1;
+    for(int j = 0 ; j < n ; j++){
+      a[n-1][j] = fib(n+br);
+      br++;
+    }
 }
 void ispis(int a[][100], int n){
     for(int i = 0 ; i < n ; i++){
@@ -27,11 +50,14 @@ void ispis(int a[][100], int n){
     }
 }
 int main(){
-    int a[100][100] = {0}, n;
+    int a[100][100] = {0}, n, i , j;
+
     scanf("%d",&n);
 
     int f = fib(n);
-    racun(a,n);
+    gore(a,n);
+    sporedna(a,n);
+    dole(a,n);
     ispis(a,n);
 
     return 0;
